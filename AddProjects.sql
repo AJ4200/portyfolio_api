29039dfafb1bdbe6beb8edf84afe9196bba96984
Abel Majadibodu <Abeljackson33@gmail.com>
2023-07-18 14:53:10 +0200

    updated sql script to add a project via query

--- AddProjects.sql
+++ AddProjects.sql
@@ -1,12 +1,11 @@
 INSERT INTO Projects (title, imgSrc, code, projectLink, description, modalContent)
-VALUES ('Portyfolio', 'project-imgs/Portyfolio.png', 'https://github.com/AJ4200/Portyfolio', 'https://portyfolio.vercel.app', 'I designed and built this website from so that one can present their resume visually. It was a birthday gift to myself', '<p>\nPortyfolio is a website that helps job seekers create an online portfolio to showcase their skills, experience, and achievements in a visually appealing way. The platform offers customizable templates and designs, allowing users to create a unique and professional-looking portfolio that reflects their personality and style. Users can add their work experience, education, skills, projects, and multimedia content to bring their portfolio to life. The platform's user-friendly interface makes it easy to edit and update the portfolio, ensuring it stays current and relevant as the user progresses in their career.\n</p>\n<p>\nPortyfolio is suitable for both recent graduates looking for their first job and experienced professionals seeking to switch careers. With the platform's visually-driven format and customizable templates, users can stand out in a crowded job market and create a compelling online brand. By signing up for Portyfolio, job seekers can take the first step towards landing their dream job by showcasing their talents and experience in a unique and creative way.\n</p>');
+VALUES ('H.D.F.Player', 'project-imgs/Hdfplayer.png', 'https://github.com/AJ4200/H.D.F.Player', 'https://h-d-f-player.vercel.app/', 'Web browser emulator that lets you play flash, html5, and jsdos games in your browser', 'H.D.F.Player is a cutting-edge web browser emulator designed to provide users with a seamless gaming experience. With the ability to play a wide range of games, including flash, html5, and jsdos, directly in your browser, H.D.F.Player revolutionizes the way you enjoy gaming. Say goodbye to the hassle of downloading and installing multiple game clients or plugins – H.D.F.Player eliminates the need for additional software by offering a comprehensive platform that supports various game formats. Whether you%apos;re a fan of classic flash games, modern html5 titles, or nostalgic jsdos gems, H.D.F.Player has got you covered. Immerse yourself in a world of endless gaming possibilities with H.D.F.Player, where convenience meets entertainment.');
  -- Get the id of the project we just inserted
-SELECT id FROM Projects WHERE title = 'Portyfolio';
+SELECT id FROM Projects WHERE title = 'H.D.F.Player';
  -- Assume the id is 1
 INSERT INTO Technologies (project_id, tech)
 VALUES 
-(1, 'Nodejs(Next)'),
-(1, 'Ruby On Rails'),
-(1, 'PostgreSQL'),
-(1, 'Express'),
-(1, 'Axios');
+(5, 'Nextjs'),
+(5, 'Ruffle'),
+(5, 'jsDOS'),
+(5, 'HTML5');
